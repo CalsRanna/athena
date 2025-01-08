fc995661a198347dc8887539a7172b2cdd6b08af
Cals <cals@CalsdeMac-mini.local>
2025-01-08 15:48:58 +0800

    refactor(schema): remove Shortcut schema from Isar initialization
    Deleted the shortcut.dart and its generated file shortcut.g.dart as they are no longer needed. Also updated the IsarInitializer to exclude ShortcutSchema from the open schemas.

--- lib/schema/isar.dart
+++ lib/schema/isar.dart
@@ -1,7 +1,6 @@
 import 'package:athena/schema/chat.dart';
 import 'package:athena/schema/model.dart';
 import 'package:athena/schema/setting.dart';
-import 'package:athena/schema/shortcut.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -11,14 +10,7 @@ class IsarInitializer {
   static Future<void> ensureInitialized() async {
     final directory = await getApplicationSupportDirectory();
     isar = await Isar.open(
-      [
-        ChatSchema,
-        MessageSchema,
-        ModelSchema,
-        SentinelSchema,
-        SettingSchema,
-        ShortcutSchema
-      ],
+      [ChatSchema, MessageSchema, ModelSchema, SentinelSchema, SettingSchema],
       directory: directory.path,
     );
   }
