db48db2bd3b7a7d24bb3ecc4f5e466a3583ffe31
Cals <cals@CalsdeMac-mini.local>
2025-01-08 15:26:21 +0800

    refactor(home): remove `workspace.dart` and integrate functionality into `home.dart`
    The `WorkSpace` widget and its associated functionality have been removed and directly integrated into the `_DesktopHomePageState` class within `home.dart`. This refactor aims to simplify the codebase by consolidating related components and avoiding unnecessary file dependencies.

--- lib/page/desktop/home/home.dart
+++ lib/page/desktop/home/home.dart
@@ -1,9 +1,10 @@
 import 'package:athena/page/desktop/home/component/chat_list.dart';
 import 'package:athena/page/desktop/home/component/indicator.dart';
+import 'package:athena/page/desktop/home/component/input.dart';
+import 'package:athena/page/desktop/home/component/message.dart';
 import 'package:athena/page/desktop/home/component/search.dart';
 import 'package:athena/page/desktop/home/component/sentinel_tile.dart';
 import 'package:athena/page/desktop/home/component/setting_tile.dart';
-import 'package:athena/page/desktop/home/component/workspace.dart';
 import 'package:athena/provider/chat.dart';
 import 'package:athena/schema/chat.dart';
 import 'package:athena/schema/model.dart';
@@ -27,22 +28,65 @@ class _DesktopHomePageState extends State<DesktopHomePage> {
   Sentinel? sentinel;
   @override
   Widget build(BuildContext context) {
-    var children = [
-      _buildLeftBar(),
-      Expanded(
-        child: WorkSpace(
-          chat: chat,
-          onSubmitted: submit,
-        ),
-      ),
-    ];
-    return AScaffold(
-      appBar: AAppBar(
-        onCreated: createChat,
-        title: DesktopChatIndicator(chat: chat),
-      ),
-      body: Row(children: children),
+    var children = [_buildLeftBar(), Expanded(child: _buildRightWorkspace())];
+    var appBar = AAppBar(
+      onCreated: createChat,
+      title: DesktopChatIndicator(chat: chat),
     );
+    return AScaffold(appBar: appBar, body: Row(children: children));
+  }
+
+  void changeChat(Chat chat) {
+    setState(() {
+      this.chat = chat;
+    });
+  }
+
+  void changeSentinel(Sentinel sentinel) {
+    var container = ProviderScope.containerOf(context);
+    var provider = chatNotifierProvider(chat?.id ?? 0);
+    var notifier = container.read(provider.notifier);
+    notifier.updateSentinel(sentinel);
+    if (chat == null) {
+      setState(() {
+        chat = Chat()..sentinelId = sentinel.id;
+        this.sentinel = sentinel;
+      });
+    } else {
+      setState(() {
+        chat = chat!.copyWith(sentinelId: sentinel.id);
+        this.sentinel = sentinel;
+      });
+    }
+  }
+
+  void createChat() {
+    setState(() {
+      chat = null;
+    });
+  }
+
+  void destroyChat() {
+    setState(() {
+      chat = null;
+    });
+  }
+
+  Future<void> submit(String text) async {
+    var container = ProviderScope.containerOf(context);
+    var provider = chatNotifierProvider(chat?.id ?? 0);
+    var notifier = container.read(provider.notifier);
+    if (chat == null) {
+      var chatId = await notifier.create();
+      setState(() {
+        chat = Chat()..id = chatId;
+      });
+    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      var provider = chatNotifierProvider(chat!.id);
+      var notifier = container.read(provider.notifier);
+      notifier.send(text, model: model, sentinel: sentinel);
+    });
   }
 
   Widget _buildLeftBar() {
@@ -68,56 +112,19 @@ class _DesktopHomePageState extends State<DesktopHomePage> {
     );
   }
 
-  void createChat() {
-    setState(() {
-      chat = null;
-    });
-  }
-
-  void destroyChat() {
-    setState(() {
-      chat = null;
-    });
-  }
-
-  void changeSentinel(Sentinel sentinel) {
-    var container = ProviderScope.containerOf(context);
-    var provider = chatNotifierProvider(chat?.id ?? 0);
-    var notifier = container.read(provider.notifier);
-    notifier.updateSentinel(sentinel);
-    if (chat == null) {
-      setState(() {
-        chat = Chat()..sentinelId = sentinel.id;
-        this.sentinel = sentinel;
-      });
-    } else {
-      setState(() {
-        chat = chat!.copyWith(sentinelId: sentinel.id);
-        this.sentinel = sentinel;
-      });
-    }
-  }
-
-  void changeChat(Chat chat) {
-    setState(() {
-      this.chat = chat;
-    });
-  }
-
-  Future<void> submit(String text) async {
-    var container = ProviderScope.containerOf(context);
-    var provider = chatNotifierProvider(chat?.id ?? 0);
-    var notifier = container.read(provider.notifier);
-    if (chat == null) {
-      var chatId = await notifier.create();
-      setState(() {
-        chat = Chat()..id = chatId;
-      });
-    }
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      var provider = chatNotifierProvider(chat!.id);
-      var notifier = container.read(provider.notifier);
-      notifier.send(text, model: model, sentinel: sentinel);
-    });
+  Widget _buildRightWorkspace() {
+    var children = [
+      Expanded(child: MessageList(chat: chat)),
+      Input(onSubmitted: submit)
+    ];
+    var column = Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: children,
+    );
+    var borderSide = BorderSide(color: Colors.white.withValues(alpha: 0.2));
+    return Container(
+      decoration: BoxDecoration(border: Border(left: borderSide)),
+      child: column,
+    );
   }
 }
