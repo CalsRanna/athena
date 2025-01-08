68ac3aa6d7b3d2195ca522ea4ff8016f89e2b840
Cals <cals@CalsdeMac-mini.local>
2025-01-08 16:21:49 +0800

    refactor(desktop home): rename search.dart to chat_search.dart and update imports
    The search.dart file was renamed to chat_search.dart to better reflect its purpose. Corresponding imports were updated in home.dart to use the new file name.

--- lib/page/desktop/home/home.dart
+++ lib/page/desktop/home/home.dart
@@ -3,7 +3,7 @@ import 'package:athena/page/desktop/home/component/chat_indicator.dart';
 import 'package:athena/page/desktop/home/component/message_input.dart';
 import 'package:athena/page/desktop/home/component/message_list.dart';
 import 'package:athena/page/desktop/home/component/sentinel_placeholder.dart';
-import 'package:athena/page/desktop/home/component/search.dart';
+import 'package:athena/page/desktop/home/component/chat_search.dart';
 import 'package:athena/page/desktop/home/component/sentinel_tile.dart';
 import 'package:athena/page/desktop/home/component/setting_tile.dart';
 import 'package:athena/provider/chat.dart';
