f6ce76afff70e4f12f95335b8680ed545b31db12
cals <calsmao@88vip.site>
2026-07-21 17:56:47 +0800

    fix(macos): handle application reopen when no visible windows

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -7,6 +7,21 @@ class AppDelegate: FlutterAppDelegate {
     return false
   }
 
+  override func applicationShouldHandleReopen(
+    _ sender: NSApplication,
+    hasVisibleWindows flag: Bool
+  ) -> Bool {
+    if !flag {
+      sender.setActivationPolicy(.regular)
+      for window in sender.windows {
+        window.setIsVisible(true)
+        window.makeKeyAndOrderFront(self)
+      }
+      sender.activate(ignoringOtherApps: true)
+    }
+    return true
+  }
+
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
